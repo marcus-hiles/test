2753fa743697fc21a77aff52ddacbdbd4e6d71bd
Marcus Hiles <39972880+marcus-hiles@users.noreply.github.com>
2018-07-31 23:12:24 -0500

    Update new.js

--- new.js
+++ new.js
@@ -1,3 +1,25 @@
 (function(){
   console.log('hi')
 })()
+
+function fakeLoad(){
+  setTimeout(() => {
+    return {
+      response: true,
+      status: 200
+    }
+  }, 3000)
+}
+
+
+(function(){
+  async function loadSomething(){
+    let load = await fakeLoad()
+    
+    if(load.status === 200){
+      return status.response 
+    }else{
+     return false 
+    }
+  }
+})()
