a14cde58da990d1a7b472627ce04945e515d4b62
Marcus Hiles <39972880+marcus-hiles@users.noreply.github.com>
2018-07-31 23:18:32 -0500

    Create vue-template.vue

---
+++ vue-template.vue
@@ -0,0 +1,27 @@
+<template>
+
+  <div id="component">
+    <div v-id="is_mobile">Hi Mobile!</div>
+  </div>
+</template>
+
+<script>
+export default {
+	data: function () {
+	  return {
+		state: {
+		  is_mobile: false,
+		  }
+		}
+	  }
+	},
+  methods: {
+  
+  }
+}
+</script>
+
+
+<style scoped>
+
+</style>
